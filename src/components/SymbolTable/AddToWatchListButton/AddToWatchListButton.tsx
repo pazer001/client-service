93d1d64166dc0c5807b17aeac458da09c7bd30f9
Ben Gedi <bengedi@gmail.com>
2025-04-24 01:26:21 +0300

    refactor: enhance AddToWatchListButton with new watchlist creation functionality and improve watchlist management in store

--- src/components/SymbolTable/AddToWatchListButton/AddToWatchListButton.tsx
+++ src/components/SymbolTable/AddToWatchListButton/AddToWatchListButton.tsx
@@ -1,42 +1,92 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { Button } from 'primereact/button'
 import { Menu } from 'primereact/menu'
 import { MenuItem } from 'primereact/menuitem'
 import { Toast } from 'primereact/toast'
 import { ISymbolItem } from '../../../stores/symbataStore.types'
 import { InputText } from 'primereact/inputtext'
+import { useWatchlistStoreActions, useWatchlistStoreWatchlists } from '../../../stores/watchlistStore'
 
 export default function AddToWatchListButton(props: ISymbolItem) {
+  const watchlists = useWatchlistStoreWatchlists()
   const menuLeft = useRef<Menu>(null)
   const toast = useRef<Toast>(null)
+
+  const addWatchlistItems = watchlists.map((watchlist) => ({
+    label: watchlist.name,
+    icon: 'pi pi-star',
+  }))
+
   const items: MenuItem[] = [
     {
-      label: 'Add to Watchlist',
+      label: 'Create new Watchlist',
       items: [
         {
-          label: 'Refresh',
-          icon: 'pi pi-refresh',
-        },
-        {
-          label: 'Export',
-          icon: 'pi pi-upload',
-        },
-      ],
-    },
-    {
-      label: 'Add Watchlist',
-      items: [
-        {
-          template: () => (
-            <div className="p-inputgroup flex-1 px-3 pb-3">
-              <InputText placeholder="Name" />
-              <Button size="small" icon="pi pi-plus" />
-            </div>
-          ),
+          template: () => {
+            const [value, setValue] = useState('')
+            const { addWatchlist } = useWatchlistStoreActions()
+            const watchlists = useWatchlistStoreWatchlists()
+            const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+              setValue(e.target.value)
+            }
+            const handleClick = () => {
+              addWatchlist(value)
+              setValue('')
+              toast.current?.show({
+                severity: 'success',
+                summary: 'Watchlist Created',
+                detail: `Watchlist ${value} created`,
+                life: 3000,
+              })
+            }
+
+            const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+              if (e.key === 'Enter') {
+                handleClick()
+              }
+            }
+            return (
+              <div className="flex flex-column gap-2 px-3 pb-3">
+                <div className="p-inputgroup flex-1">
+                  <InputText
+                    placeholder="Name"
+                    name="watchlist"
+                    id="watchlist"
+                    aria-describedby="watchlist-help"
+                    value={value}
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
+                    className="w-full"
+                  />
+                  <Button
+                    size="small"
+                    icon="pi pi-plus"
+                    disabled={value.length === 0 || watchlists.some((watchlist) => watchlist.name === value)}
+                    onClick={handleClick}
+                  />
+                </div>
+                {watchlists.some((watchlist) => watchlist.name === value) && (
+                  <small id="watchlist-help" className="text-red-300">
+                    Watchlist already exists
+                  </small>
+                )}
+              </div>
+            )
+          },
         },
       ],
     },
   ]
+
+  if (addWatchlistItems.length > 0) {
+    items.unshift(
+      {
+        label: 'Add to Watchlist',
+        items: addWatchlistItems,
+      },
+      { separator: true },
+    )
+  }
   console.log('AddToWatchListButton', props)
 
   return (
@@ -44,8 +94,9 @@ export default function AddToWatchListButton(props: ISymbolItem) {
       <Toast ref={toast}></Toast>
       <Menu className="w-full md:w-15rem" model={items} popup ref={menuLeft} id="popup_menu_left" />
       <Button
-        icon="pi pi pi-ellipsis-v"
+        icon="pi pi-star"
         text
+        severity="secondary"
         size="small"
         onClick={(event) => menuLeft?.current?.toggle(event)}
         aria-controls="popup_menu_left"
--- src/stores/watchlistStore.ts
+++ src/stores/watchlistStore.ts
@@ -1,10 +1,12 @@
 import { create, StateCreator } from 'zustand'
-import { devtools } from 'zustand/middleware'
+import { persist, createJSONStorage } from 'zustand/middleware'
 import { ISymbolItem } from './symbataStore.types.ts'
 
 interface IWatchListStoreActions {
-  addWatchlist: (name: string) => Promise<void>
-  removeWatchlist: (name: string) => Promise<void>
+  addWatchlist: (watchlistName: string) => Promise<void>
+  addToWatchlist: (watchlistName: string, symbol: ISymbolItem) => Promise<void>
+  removeWatchlist: (watchlistName: string) => Promise<void>
+  removeFromWatchlist: (watchlistName: string, symbol: ISymbolItem) => Promise<void>
 }
 
 interface IWatchlist {
@@ -13,17 +15,25 @@ interface IWatchlist {
 }
 
 interface IWatchListStore {
-  currentWatchlist: string | undefined
+  currentWatchlist: IWatchlist | null
   watchlists: IWatchlist[]
   actions: IWatchListStoreActions
 }
 
-const watchlistStore: StateCreator<IWatchListStore> = () => ({
-  currentWatchlist: undefined,
+const watchlistStore: StateCreator<IWatchListStore> = (set) => ({
+  currentWatchlist: null,
   watchlists: [],
   actions: {
     addWatchlist: async (name: string) => {
-      console.log('Adding watchlist:', name)
+      set((state) => ({
+        watchlists: [...state.watchlists, { name, symbols: [] }],
+      }))
+    },
+    addToWatchlist: async (name: string, symbol: ISymbolItem) => {
+      console.log('Adding symbol to watchlist:', name, symbol)
+    },
+    removeFromWatchlist: async (name: string, symbol: ISymbolItem) => {
+      console.log('Removing symbol from watchlist:', name, symbol)
     },
     removeWatchlist: async (name: string) => {
       console.log('Removing watchlist:', name)
@@ -31,7 +41,15 @@ const watchlistStore: StateCreator<IWatchListStore> = () => ({
   },
 })
 
-const useWatchlistStore = create<IWatchListStore>()(devtools(watchlistStore, { name: 'watchlistStore' }))
+const useWatchlistStore = create<IWatchListStore>()(
+  persist(watchlistStore, {
+    name: 'watchlistStore',
+    storage: createJSONStorage(() => localStorage),
+    partialize: (state) => ({
+      watchlists: state.watchlists,
+    }),
+  }),
+)
 export const useWatchlistStoreActions = () => useWatchlistStore((state) => state.actions)
 export const useWatchlistStoreCurrentWatchlist = () => useWatchlistStore((state) => state.currentWatchlist)
 export const useWatchlistStoreWatchlists = () => useWatchlistStore((state) => state.watchlists)
