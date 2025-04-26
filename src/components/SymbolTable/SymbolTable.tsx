705494d7e98c1cfc2567d291d37f51c8a93151dd
Ben Gedi <bengedi@gmail.com>
2025-04-26 16:08:57 +0300

    refactor: enable autoSize in ResponsiveGridLayout and remove showGridlines from DataTable for improved layout

--- src/components/SymbolTable/SymbolTable.tsx
+++ src/components/SymbolTable/SymbolTable.tsx
@@ -28,7 +28,6 @@ export const SymbolTable = () => {
         >
           <DataTable
             removableSort
-            showGridlines
             scrollable
             pt={{
               root: { className: 'h-full' },
