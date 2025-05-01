776ce4c95cc875d94c9a5638e4f519d56355487a
Ben Gedi <bengedi@gmail.com>
2025-05-01 17:32:14 +0300

    fix: update CustomTabPanel declaration to improve clarity and context

--- src/components/SymbolTable/SymbolTable.tsx
+++ src/components/SymbolTable/SymbolTable.tsx
@@ -22,7 +22,8 @@ interface TabPanelProps {
 
 // this is an example code from MUI documentation
 // https://mui.com/material-ui/react-tabs/#introduction (first example)
-function CustomTabPanel(props: TabPanelProps) {
+// the Mui TabPanel component has to much padding by default, so we need to create our own, since the table don't don't have enough space
+const CustomTabPanel = (props: TabPanelProps) => {
   const { children, value, index, ...other } = props
 
   return (
