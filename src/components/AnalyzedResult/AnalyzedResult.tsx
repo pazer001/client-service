82e556fbb1da240fb012f84eccfd08fbab4ec02a
Ben Gedi <bengedi@gmail.com>
2025-12-06 15:42:10 +0200

    Refactor AnalyzedResult component by removing unused imports and simplifying logic
    Cleaned up the AnalyzedResult component by eliminating unnecessary imports and functions, streamlining the code for better readability and maintainability. Updated mock data generation to facilitate testing while ensuring production readiness. The component now focuses on essential functionalities, enhancing overall performance.

--- src/components/AnalyzedResult/AnalyzedResult.tsx
+++ src/components/AnalyzedResult/AnalyzedResult.tsx
@@ -1,126 +1,12 @@
 import FilterListIcon from '@mui/icons-material/FilterList'
-import {
-  Avatar,
-  Box,
-  Card,
-  CardContent,
-  Chip,
-  FormControl,
-  IconButton,
-  Menu,
-  MenuItem,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableRow,
-  TextField,
-  Typography,
-} from '@mui/material'
-import { startCase } from 'lodash'
-import { ReactElement, useEffect, useRef, useState } from 'react'
+import { Box, Card, CardContent, Chip, IconButton, Menu, MenuItem, Paper, Typography } from '@mui/material'
+import { useEffect, useRef, useState } from 'react'
 import { Virtuoso, VirtuosoHandle } from 'react-virtuoso'
 import { io, Socket } from 'socket.io-client'
-
-import {
-  useSymbataStoreActions,
-  useSymbataStoreProfileValue,
-  useSymbataStoreSymbol,
-  useSymbataStoreUserId,
-} from '../../stores/symbataStore.ts'
-import { EAction, ISymbolItem } from '../../stores/symbataStore.types.ts'
-import { formatNumber, getShares } from '../../utils/utils.ts'
-
-function createData(label: string, value: number | string | undefined, renderCell?: () => ReactElement) {
-  return { label, value, renderCell }
-}
-
-const ActionLabel = (action: EAction): ReactElement => {
-  switch (action) {
-    case EAction.BUY:
-      return <Typography color="success">Buy</Typography>
-    case EAction.SELL:
-      return <Typography color="error">Sell</Typography>
-    case EAction.HOLD:
-      return <Typography>Hold</Typography>
-    case EAction.ERROR:
-      return <Typography color="warning">Error</Typography>
-    default:
-      return <Typography color="info">Unknown Action</Typography>
-  }
-}
+import { useSymbataStoreUserId } from '../../stores/symbataStore'
 
 const AnalyzedResult = () => {
-  const symbol: ISymbolItem | undefined = useSymbataStoreSymbol()
-  const profileValue = useSymbataStoreProfileValue()
-  const { setProfileValue } = useSymbataStoreActions()
-
-  const closeValue =
-    symbol?.recommendation?.symbolRestructurePrices.close[
-      symbol?.recommendation?.symbolRestructurePrices.close.length - 1
-    ] ?? 0
-  const stopLoss = symbol?.recommendation?.stopLoss ?? 0
-  const stopLossDifference = closeValue - stopLoss
-
-  const stopLossPercentage = (stopLossDifference * 100) / closeValue
-  const shares = getShares(profileValue, symbol?.recommendation?.stopLoss ?? 0, 0.02, closeValue)
-
-  const initRows = [
-    createData('Symbol', symbol?.symbol, () => (
-      <Box height={'100%'} display="flex" alignItems="center" gap={1}>
-        <Avatar sx={{ width: 24, height: 24 }} src={symbol?.logo}>
-          {symbol?.symbol.charAt(0).toUpperCase()}
-        </Avatar>
-        <Typography>{symbol?.symbol}</Typography>
-      </Box>
-    )),
-    createData('Company Name', symbol?.name),
-    createData('Action', symbol?.recommendation?.action, () =>
-      ActionLabel(symbol?.recommendation?.action ?? EAction.ERROR),
-    ),
-  ]
-
-  const rows =
-    symbol?.recommendation?.action === EAction.BUY
-      ? [
-          ...initRows,
-          createData('Stop Loss', `${formatNumber(stopLossPercentage)}%`),
-          createData('Buy Shares', shares),
-          createData('Strategy', startCase(symbol.recommendation.usedStrategy)),
-        ]
-      : initRows
   return <Messages />
-  return (
-    <Box display="flex" flexDirection="column" gap={1}>
-      <Messages />
-      <FormControl>
-        <TextField
-          size="small"
-          type="number"
-          id="component-outlined"
-          value={profileValue}
-          onChange={(e) => setProfileValue(Number(e.target.value))}
-          label="Profile Value"
-        />
-      </FormControl>
-      {symbol?.recommendation ? (
-        <Table size="small" aria-label="a dense table">
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.label}>
-                <TableCell component="th" scope="row">
-                  {row.label}
-                </TableCell>
-                <TableCell>{row?.renderCell ? row?.renderCell() : row.value}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      ) : (
-        <Typography>No symbol selected</Typography>
-      )}
-    </Box>
-  )
 }
 
 interface LogMessage {
@@ -308,7 +194,6 @@ const Messages = () => {
   return (
     <Card
       elevation={3}
-      className="bengedi"
       sx={{
         borderRadius: 2,
         display: 'flex',
@@ -465,10 +350,9 @@ const Messages = () => {
                   />
                 ),
               }}
-              itemContent={(index, msg) => (
+              itemContent={(_index, msg) => (
                 <Paper
                   elevation={0}
-                  key={index}
                   sx={{
                     p: 1,
                     mb: 1,
